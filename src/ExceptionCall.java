0977a4059be440c669727cb85a0666d1fa0cbc17
parmarviral <parmarviral93@gmail.com>
2019-01-20 10:59:37 +0530

    Formatting

--- src/ExceptionCall.java
+++ src/ExceptionCall.java
@@ -1,22 +1,26 @@
 class MyException extends Exception {
     private int detail;
+
     MyException(int a) {
         detail = a;
     }
+
     public String toString() {
         return "Division by zero ";
     }
 }
+
 class ExceptionClass {
     static void compute(int a) throws MyException {
-        if(a == 0)
+        if (a == 0)
             throw new MyException(a);
-        int b = 40/a;
+        int b = 40 / a;
         System.out.println(b);
     }
+
     public static void main(String args[]) {
         try {
-            int l= args.length;
+            int l = args.length;
             compute(l);
         } catch (MyException e) {
             System.out.println("Caught: " + e);
--- src/Project/Banking_App.java
+++ src/Project/Banking_App.java
@@ -1,22 +1,28 @@
 package Project;
+
 class MyException extends Exception {
     private int detail;
+
     MyException(int a) {
         detail = a;
     }
+
     public String toString() {
         return "Not sufficient fund";
     }
 }
-class Banking_App{
-    static int t=1000;
+
+class Banking_App {
+    static int t = 1000;
+
     static void compute(int a) throws MyException {
 //System.out.println("Called compute(" + a + ")");
-        t=t-a;
-        if(t < 0)
+        t = t - a;
+        if (t < 0)
             throw new MyException(a);
         System.out.println("remaining balance is:" + t);
     }
+
     public static void main(String args[]) {
         try {
             compute(400);
